df87b0bcf517fa3270462705406e38ac9247774d
ZhangYanHui <27775701@qq.com>
2022-06-04 17:52:44 +0800

    no message

---
+++ 案例 30 在animals和CIFAR-10上使用浅层网络/shallownet.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+class ShallowNet:
+	@staticmethod
+	def build(width, height, depth, classes):
+		# initialize the model along with the input shape to be
+		# "channels last"
+		model = Sequential()
+		inputShape = (height, width, depth)
+		# if we are using "channels first", update the input shape
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+        # define the first (and only) CONV => RELU layer
+		model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
+		model.add(Activation("relu"))
+		# softmax classifier
+		model.add(Flatten())
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+		# return the constructed network architecture
+		return model
