95b999926d5144a8ca7a5d7dd179c51cf578e9f7
ZhangYanHui <27775701@qq.com>
2022-06-04 21:52:42 +0800

    no message

---
+++ 案例40 ResNet50提取猫狗大战特征，后用逻辑回归/customize/tools/fcheadnet.py
@@ -0,0 +1,21 @@
+# import the necessary packages
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+
+# 构建head
+class FCHeadNet:
+    @staticmethod
+    def build(baseModel, classes, D):
+        # initialize the head model that will be placed on top of
+        # the base, then add a FC layer
+        headModel = baseModel.output
+        headModel = Flatten(name="flatten")(headModel)
+        headModel = Dense(D, activation="relu")(headModel)
+        headModel = Dropout(0.5)(headModel)
+
+        # add a softmax layer
+        headModel = Dense(classes, activation="softmax")(headModel)
+
+        # return the model
+        return headModel
