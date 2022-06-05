5dd47eead33c48318cafe29c33f537adde70b3f5
ZhangYanHui <27775701@qq.com>
2022-06-05 09:20:37 +0800

    no message

---
+++ 案例63 cycle 莫奈风格的画/README.md
@@ -0,0 +1,9 @@
+## 简述
+Kaggle竞赛 生成莫奈风格图像的GAN
+
+## csdn文章
+https://skydance.blog.csdn.net/article/details/124168736
+
+## 数据集下载
+链接：https://pan.baidu.com/s/1aktUXmy7QG50UjeQGsMbLg 
+提取码：f1t4
---
+++ 案例63 cycle 莫奈风格的画/t_gan.py
@@ -0,0 +1,485 @@
+import os, re
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+#from kaggle_datasets import KaggleDatasets
+import seaborn as sns
+import pylab
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+print(tf.__version__)
+
+BATCH = 2
+EPOCHS = 1
+lr = 0.002
+
+
+######################## 访问 TF 记录数据图像
+MONET_FILENAMES = tf.io.gfile.glob(str('D:/gan-getting-started/monet_tfrec/*.tfrec'))
+PHOTO_FILENAMES = tf.io.gfile.glob(str('D:/gan-getting-started/photo_tfrec/*.tfrec'))
+
+# count number of images in all tfrec files
+def count_data_items(filenames):
+    # Regular expression to group files
+    item = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1)) for filename in filenames]
+    return np.sum(item)
+
+monet_samples = count_data_items(MONET_FILENAMES)
+photo_samples = count_data_items(PHOTO_FILENAMES)
+
+print('Monet TFRecord Files:', len(MONET_FILENAMES))
+print('Monet TFRecord Images:', monet_samples)
+print('Photo TFRecord Files:', len(PHOTO_FILENAMES))
+print('Photo TFRecord Images:', photo_samples)
+
+AUTO = tf.data.experimental.AUTOTUNE
+
+for i in MONET_FILENAMES:
+    if 'D:\\gan-getting-started\\monet_tfrec\\' in i:
+        print(i.replace('D:\\gan-getting-started\\monet_tfrec\\',''))
+    else:
+        print(i)
+
+IMAGE_SIZE = [256, 256]
+
+
+def decode_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = (tf.cast(image, dtype=tf.float32) / 127.5) - 1  # Map values in the range [-1, 1]
+    image = tf.reshape(image, [256, 256, 3])
+    return image
+
+
+def read_tfrecord(example):
+    tfrecord_format = {"image_name": tf.io.FixedLenFeature([], tf.string),
+                       "image": tf.io.FixedLenFeature([], tf.string),
+                       "target": tf.io.FixedLenFeature([], tf.string)}
+    example = tf.io.parse_single_example(example, tfrecord_format)
+    image = decode_image(example['image'])
+    return image
+
+def load_dataset(filenames):
+    dataset = tf.data.TFRecordDataset(filenames)
+    dataset = dataset.map(read_tfrecord, num_parallel_calls=AUTOTUNE)
+
+    #dataset = dataset.take(2).cache().batch(BATCH).repeat().prefetch(AUTO)
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(512)
+    dataset = dataset.batch(BATCH)
+    dataset = dataset.cache()
+    dataset = dataset.prefetch(AUTO)
+    return dataset
+
+monet_ds = load_dataset(MONET_FILENAMES)
+photo_ds = load_dataset(PHOTO_FILENAMES)
+
+example_monet = next(iter(monet_ds))
+example_photo = next(iter(photo_ds))
+
+# Example of 2 images
+plt.subplot(121)
+plt.title('Photo')
+plt.imshow(example_photo[0] * 0.5 + 0.5) # To map values between [0,1]
+
+
+plt.subplot(122)
+plt.title('Monet')
+plt.imshow(example_monet[0] * 0.5 + 0.5) # To map values between [0,1]
+pylab.show()
+
+######################## Build the generator
+
+OUTPUT_CHANNELS = 3
+
+def downsample(filters, size, strides, apply_instancenorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+
+    result = keras.Sequential()
+    result.add(layers.Conv2D(filters, size, strides=strides,
+                             padding='same',
+                             kernel_initializer=initializer,
+                             use_bias=False))
+
+    if apply_instancenorm:
+        result.add(tfa.layers.InstanceNormalization(gamma_initializer=gamma_init))
+
+    result.add(layers.LeakyReLU())
+
+    return result
+
+
+def upsample(filters, size, strides, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+
+    result = keras.Sequential()
+    result.add(layers.Conv2DTranspose(filters, size, strides=strides,
+                                      padding='same',
+                                      kernel_initializer=initializer,
+                                      use_bias=False))
+
+    result.add(tfa.layers.InstanceNormalization(gamma_initializer=gamma_init))
+
+    if apply_dropout:
+        result.add(layers.Dropout(0.5))
+
+    result.add(layers.ReLU())
+
+    return result
+
+def transformer(filters, size, strides):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+
+    result = keras.Sequential()
+    result.add(layers.Conv2D(filters, size, strides=strides,
+                                      padding='same',
+                                      kernel_initializer=initializer,
+                                      use_bias=False))
+
+    result.add(tfa.layers.InstanceNormalization(gamma_initializer=gamma_init))
+
+    result.add(layers.ReLU())
+
+    return result
+
+
+def Generator():
+    inputs = layers.Input(shape=[256, 256, 3])
+
+    # bs = batch size
+    down_stack = [downsample(64, 7, 1, apply_instancenorm=False),
+                  downsample(128, 3, 2),
+                  downsample(256, 3, 2)]
+
+    transformer_stack = [transformer(256, 3, 1),
+                         transformer(256, 3, 1),
+                         transformer(256, 3, 1),
+                         transformer(256, 3, 1),
+                         transformer(256, 3, 1),
+                         transformer(256, 3, 1), ]
+
+    up_stack = [upsample(128, 3, 2, apply_dropout=True),
+                upsample(64, 3, 2, apply_dropout=True)]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = layers.Conv2D(OUTPUT_CHANNELS, 7,
+                         strides=1,
+                         padding='same',
+                         kernel_initializer=initializer,
+                         activation='tanh')
+    x = inputs
+
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+    skips = reversed(skips[:-1])
+
+    # Transformer
+    for trans in transformer_stack:
+        x = trans(x)
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = layers.Concatenate()([x, skip])
+
+    x = last(x)
+
+    return keras.Model(inputs=inputs, outputs=x)
+
+sample_generator = Generator()
+tf.keras.utils.plot_model(sample_generator, show_shapes=True, dpi=64)
+
+
+###############  Build the Discriminator
+def Discriminator():
+    initializer = tf.random_normal_initializer(0., 0.02)
+    gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+
+    inp = layers.Input(shape=[256, 256, 3], name='input_image')
+
+    x = inp
+
+    down1 = downsample(64, 4, 2)(x)
+    down2 = downsample(128, 4, 2)(down1)
+    down3 = downsample(256, 4, 2)(down2)
+    down4 = downsample(512, 4, 2)(down3)
+
+    conv = layers.Conv2D(1, 4, strides=1,
+                         kernel_initializer=initializer,
+                         use_bias=False)(down4)
+    out = tf.keras.activations.sigmoid(conv)
+
+    return tf.keras.Model(inputs=inp, outputs=out)
+
+sample_dis = Discriminator()
+tf.keras.utils.plot_model(sample_dis, show_shapes=True, dpi=64)
+
+with strategy.scope():
+    monet_generator = Generator() # transforms photos to Monet-esque paintings
+    photo_generator = Generator() # transforms Monet paintings to be more like photos
+
+    monet_discriminator = Discriminator() # differentiates real Monet paintings and generated Monet paintings
+    photo_discriminator = Discriminator() # differentiates real photos and generated photos
+
+to_monet = monet_generator(example_photo)
+
+plt.subplot(1, 2, 1)
+plt.title("Original Photo")
+plt.imshow(example_photo[0] * 0.5 + 0.5)
+
+plt.subplot(1, 2, 2)
+plt.title("Monet-esque Photo")
+plt.imshow(to_monet[0] * 0.5 + 0.5)
+plt.show()
+
+#################     Define Loss
+with strategy.scope():
+    def discriminator_loss(real, generated):
+        real_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)(tf.ones_like(real), real)
+        generated_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)(tf.zeros_like(generated), generated)
+        total_disc_loss = real_loss + generated_loss
+        return total_disc_loss * 0.5
+
+with strategy.scope():
+    def generator_loss(generated):
+        return tf.keras.losses.BinaryCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)(tf.ones_like(generated), generated)
+
+with strategy.scope():
+    def calc_cycle_loss(real_image, cycled_image, LAMBDA):
+        loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
+        return LAMBDA * loss1
+
+with strategy.scope():
+    def identity_loss(real_image, same_image, LAMBDA):
+        loss = tf.reduce_mean(tf.abs(real_image - same_image))
+        return LAMBDA * 0.5 * loss
+
+#############################Build the GAN model
+class CycleGan(keras.Model):
+    def __init__(
+            self,
+            monet_generator,
+            photo_generator,
+            monet_discriminator,
+            photo_discriminator,
+            lambda_cycle=10,
+    ):
+        super(CycleGan, self).__init__()
+        self.m_gen = monet_generator
+        self.p_gen = photo_generator
+        self.m_disc = monet_discriminator
+        self.p_disc = photo_discriminator
+        self.lambda_cycle = lambda_cycle
+
+    def compile(
+            self,
+            m_gen_optimizer,
+            p_gen_optimizer,
+            m_disc_optimizer,
+            p_disc_optimizer,
+            gen_loss_fn,
+            disc_loss_fn,
+            cycle_loss_fn,
+            identity_loss_fn
+    ):
+        super(CycleGan, self).compile()  # keras.model.compile()
+        self.m_gen_optimizer = m_gen_optimizer
+        self.p_gen_optimizer = p_gen_optimizer
+        self.m_disc_optimizer = m_disc_optimizer
+        self.p_disc_optimizer = p_disc_optimizer
+        self.gen_loss_fn = gen_loss_fn
+        self.disc_loss_fn = disc_loss_fn
+        self.cycle_loss_fn = cycle_loss_fn
+        self.identity_loss_fn = identity_loss_fn
+
+    def train_step(self, batch_data):
+        real_monet, real_photo = batch_data
+
+        with tf.GradientTape(persistent=True) as tape:
+            # photo to monet back to photo
+            fake_monet = self.m_gen(real_photo, training=True)
+            cycled_photo = self.p_gen(fake_monet, training=True)
+
+            # monet to photo back to monet
+            fake_photo = self.p_gen(real_monet, training=True)
+            cycled_monet = self.m_gen(fake_photo, training=True)
+
+            # generating itself
+            same_monet = self.m_gen(real_monet, training=True)
+            same_photo = self.p_gen(real_photo, training=True)
+
+            # discriminator used to check, inputing real images
+            disc_real_monet = self.m_disc(real_monet, training=True)
+            disc_real_photo = self.p_disc(real_photo, training=True)
+
+            # discriminator used to check, inputing fake images
+            disc_fake_monet = self.m_disc(fake_monet, training=True)
+            disc_fake_photo = self.p_disc(fake_photo, training=True)
+
+            # evaluates generator loss
+            monet_gen_loss = self.gen_loss_fn(disc_fake_monet)
+            photo_gen_loss = self.gen_loss_fn(disc_fake_photo)
+
+            # evaluates total cycle consistency loss
+            total_cycle_loss = self.cycle_loss_fn(real_monet, cycled_monet, self.lambda_cycle) + self.cycle_loss_fn(
+                real_photo, cycled_photo, self.lambda_cycle)
+
+            # evaluates total generator loss
+            total_monet_gen_loss = monet_gen_loss + total_cycle_loss + self.identity_loss_fn(real_monet, same_monet,
+                                                                                             self.lambda_cycle)
+            total_photo_gen_loss = photo_gen_loss + total_cycle_loss + self.identity_loss_fn(real_photo, same_photo,
+                                                                                             self.lambda_cycle)
+
+            # evaluates discriminator loss
+            monet_disc_loss = self.disc_loss_fn(disc_real_monet, disc_fake_monet)
+            photo_disc_loss = self.disc_loss_fn(disc_real_photo, disc_fake_photo)
+
+        # Calculate the gradients for generator and discriminator
+        monet_generator_gradients = tape.gradient(total_monet_gen_loss,
+                                                  self.m_gen.trainable_variables)
+        photo_generator_gradients = tape.gradient(total_photo_gen_loss,
+                                                  self.p_gen.trainable_variables)
+
+        monet_discriminator_gradients = tape.gradient(monet_disc_loss,
+                                                      self.m_disc.trainable_variables)
+        photo_discriminator_gradients = tape.gradient(photo_disc_loss,
+                                                      self.p_disc.trainable_variables)
+
+        # Apply the gradients to the optimizer
+        self.m_gen_optimizer.apply_gradients(zip(monet_generator_gradients,
+                                                 self.m_gen.trainable_variables))
+
+        self.p_gen_optimizer.apply_gradients(zip(photo_generator_gradients,
+                                                 self.p_gen.trainable_variables))
+
+        self.m_disc_optimizer.apply_gradients(zip(monet_discriminator_gradients,
+                                                  self.m_disc.trainable_variables))
+
+        self.p_disc_optimizer.apply_gradients(zip(photo_discriminator_gradients,
+                                                  self.p_disc.trainable_variables))
+
+        return {
+            "monet_gen_loss": total_monet_gen_loss,
+            "photo_gen_loss": total_photo_gen_loss,
+            "monet_disc_loss": monet_disc_loss,
+            "photo_disc_loss": photo_disc_loss
+        }
+
+
+##################   Learning Rate schedule
+@tf.function
+def linear_schedule_with_warmup(step):
+    """ Create a schedule with a learning rate that decreases linearly after
+    linearly increasing during a warmup period.
+    """
+    lr_start = 2e-4
+    lr_max = 2e-4
+    lr_min = 0.0001
+
+    steps_per_epoch = int(max(monet_samples, photo_samples) // BATCH)
+    total_steps = EPOCHS * steps_per_epoch
+    warmup_steps = 1
+    hold_max_steps = total_steps * 0.60
+
+    if step < warmup_steps:
+        lr = (lr_max - lr_start) / warmup_steps * step + lr_start
+    elif step < warmup_steps + hold_max_steps:
+        lr = lr_max
+    else:
+        lr = lr_max * ((total_steps - step) / (total_steps - warmup_steps - hold_max_steps))
+        if lr_min is not None:
+            #             lr = max(lr_min, lr)
+            lr = tf.math.maximum(lr_min, lr)
+
+    #     print('lr: '+ str(round(lr,5)))
+
+    #     return round(lr,5)
+    return lr
+
+steps_per_epoch = int(max(monet_samples, photo_samples) // BATCH)
+total_steps = EPOCHS * steps_per_epoch
+rng = [i for i in range(0, total_steps, 50)]
+y = [linear_schedule_with_warmup(x) for x in rng]
+
+sns.set(style="whitegrid")
+fig, ax = plt.subplots(figsize=(20, 6))
+plt.plot(rng, y)
+print(f'{EPOCHS} total epochs and {steps_per_epoch} steps per epoch')
+print(f'Learning rate schedule: {y[0]:.3g} to {max(y):.3g} to {y[-1]:.3g}')
+
+
+################# Define Optimizers
+with strategy.scope():
+    #     lr_monet_gen, lr_photo_gen, lr_monet_disc, lr_photo_disc = lr, lr, lr, lr
+    lr_monet_gen = lambda: linear_schedule_with_warmup(tf.cast(monet_generator_optimizer.iterations, tf.float32))
+    lr_photo_gen = lambda: linear_schedule_with_warmup(tf.cast(photo_generator_optimizer.iterations, tf.float32))
+
+    monet_generator_optimizer = tf.keras.optimizers.Adam(lr_monet_gen, beta_1=0.5)
+    photo_generator_optimizer = tf.keras.optimizers.Adam(lr_photo_gen, beta_1=0.5)
+
+    lr_monet_disc = lambda: linear_schedule_with_warmup(tf.cast(monet_discriminator_optimizer.iterations, tf.float32))
+    lr_photo_disc = lambda: linear_schedule_with_warmup(tf.cast(photo_discriminator_optimizer.iterations, tf.float32))
+
+    monet_discriminator_optimizer = tf.keras.optimizers.Adam(lr_monet_disc, beta_1=0.5)
+    photo_discriminator_optimizer = tf.keras.optimizers.Adam(lr_photo_disc, beta_1=0.5)
+
+
+##########  Trainnig GAN cycle
+with strategy.scope():
+    cycle_gan_model = CycleGan(
+        monet_generator, photo_generator, monet_discriminator, photo_discriminator
+    )
+
+    cycle_gan_model.compile(
+        m_gen_optimizer = monet_generator_optimizer,
+        p_gen_optimizer = photo_generator_optimizer,
+        m_disc_optimizer = monet_discriminator_optimizer,
+        p_disc_optimizer = photo_discriminator_optimizer,
+        gen_loss_fn = generator_loss,
+        disc_loss_fn = discriminator_loss,
+        cycle_loss_fn = calc_cycle_loss,
+        identity_loss_fn = identity_loss)
+
+gan_ds=tf.data.Dataset.zip((monet_ds, photo_ds))
+
+cycle_gan_model.fit(gan_ds,
+                    epochs = EPOCHS,
+                    verbose = 1,
+                    steps_per_epoch=(max(monet_samples, photo_samples)//BATCH))
+
+########## Visualize Data
+
+_, ax = plt.subplots(5, 2, figsize=(40, 40))
+for i, img in enumerate(photo_ds.take(5)):
+    prediction = monet_generator(img, training=False)[0].numpy()
+    prediction = (prediction * 127.5 + 127.5).astype(np.uint8)
+    img = (img[0] * 127.5 + 127.5).numpy().astype(np.uint8)
+    ax[i, 0].imshow(img)
+    ax[i, 1].imshow(prediction)
+    ax[i, 0].set_title("Input Photo")
+    ax[i, 1].set_title("Monet-esque")
+    ax[i, 0].axis("off")
+    ax[i, 1].axis("off")
+plt.show()
+
+
